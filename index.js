791382f2fabb55819246b192c6fd4e33d1ade982
Mark Nosal <mark.c.nosal@gmail.com>
2023-01-11 21:39:33 -0600

    Finished the lab!

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  let result;
+  if (rideDistance <= 400) {
+    result = 'This one is on me!';
+  } else if (rideDistance > 400) {
+      if (rideDistance < 2000) {
+        result = 'That will be twenty bucks.';
+      } else if (rideDistance > 2000) {
+        if (rideDistance > 2500) {
+          result = 'No can do.';
+        } else {
+          result = 'I will gladly take your thirty bucks.';
+        }
+      }
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result;
+  result = (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result;
+  switch (tip) {
+    case tip = 'generous':
+      result = 'Thank you so much.'
+      break;
+    case tip ='not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.';
+  }
+  return result;
 }
